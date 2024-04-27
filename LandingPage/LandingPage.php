33e507a484e6102ff64cff128f3b456a7b2a3ad3
ADR301 <151512964+ADR301@users.noreply.github.com>
2024-04-27 15:40:23 +0800

    Uploaded the Home Page

---
+++ LandingPage/LandingPage.php
@@ -0,0 +1,180 @@
+<?php ?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Cabalen</title>
+        <link rel="shortcut icon" href="https://raw.githubusercontent.com/dejaaay/Cabelen/main/assets/img/Cabalen.png">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"> 
+        <!-- Latest compiled JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Bootstrap CSS -->
+        <link href="css/index.css" rel="stylesheet" />
+        <link href="css/index_newsletter.css" rel="stylesheet" />
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <?php include "navbar.php"; ?>
+        <!-- Greetings card na naka carousell part -->
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="card text-white">
+                        <img src="https://mnltoday.ph/wp-content/uploads/2019/03/hot-air-balloon.jpg" class="card-img" alt="First slide">
+                        <div class="card-img-overlay">
+                            <h1 class="card-title1">Hot Air Balloon Festival</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item ">
+                    <div class="card text-white">
+                        <img src="https://gttp.imgix.net/308790/x/0/?auto=format%2Ccompress&crop=faces%2Cedges%2Ccenter&bg=%23fff&fit=crop&q=35&h=504&dpr=1" class="card-img" alt="second slide">
+                        <div class="card-img-overlay">
+                            <h1 class="card-title1">Mt. Pinatubo Crater Lake</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item ">
+                <div class="card text-white">
+                        <img src="https://i0.wp.com/www.angsarap.net/wp-content/uploads/2019/11/Bringhe-Wide.jpg?ssl=1" class="card-img" alt="third slide">
+                        <div class="card-img-overlay">
+                            <h1 class="card-title1">Bringhe</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item ">
+                    <div class="card text-white">
+                        <img src="http://www.traveltothephilippines.info/wp-content/uploads/2012/06/Pampanga-Sinukwan-Festival4.jpg" class="card-img" alt="fourth slide">
+                        <div class="card-img-overlay">
+                            <h1 class="card-title1">Sinukwan Festival</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item ">
+                    <div class="card text-white">
+                        <img src="https://2.bp.blogspot.com/-Om3GfjfF9Sw/XfgPi5uI6rI/AAAAAAAA80A/-xFymE-ZFtQXD195bPp5RFdG-epwezE-gCLcBGAsYHQ/s1600/79371861_1062324827444680_1454270166874456064_n.jpg" class="card-img" alt="Fifth slide">
+                        <div class="card-img-overlay">
+                            <h1 class="card-title1">Giant Lantern Festival</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+        <br>
+        <!-- about the page -->
+        <!-- Content Section 1 -->
+        <section id="scroll">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-1"><img class="img-fluid" src="assets/img/HotAirBalloonClark01.jpg" alt="Cabalen Image 1"/></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-1">
+                            <h2 class="display-4">Explore Pampanga</h2>
+                            <p class="txt">Pampanga is waving and is ready to welcome you open arms! Experience its rich culture, mouthwatering foods and exciting attractions and tourist destinations. 
+                                Our website is designed for everyone who is looking forward to enrich themselves in all the facets of Pampanga. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br>
+        <br>
+    <!-- Content Section 2 -->
+    <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-1"><img class="img-fluid" src="assets/img/LigliganParul01.jpg" alt="Cabalen Image 2"/></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-1">
+                            <h1 class="display-4">Learn about the Culture</h1>
+                            <p class="txt">Not knowing where and what to eat? Our blog section will convince you why Pampanga is truly the Culinary Capital of the Philippines.  
+                                There you will indulge a wide variety of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, or deep fried stuffed frog, to their Kamaru, or mole crickets! 
+                                Tara! Mangan Tamu!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br>
+        <!--Video part--> 
+    <iframe 
+        class= "center"
+        width=100% 
+        height="900" 
+        src="https://www.youtube.com/embed/-3k-SC22zbA?autoplay=1" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+    </iframe>
+    <br>
+        <!--News And Blogs-->
+        <hr>
+        <h1 class="display-4 text-center">News</h1>
+        <br>
+            <div class= "container">
+                <div class= "row">
+                    <div class= "col">
+                        <div class="card border-secondary bg-success mb-3" style="width: 25rem; height: auto;">
+                        <img src="https://kapampangan.org/wp-content/uploads/2019/06/Pampanga-clay-making-pottery.png" class="card-img-top" alt="pampanga art">      
+                        <div class="card-body text-white">
+                                <h4 class="card-title">Kapampangan Art: The best in the world</h4>
+                                <p class="card-text">There is no doubt that Pampanga has a very rich culture and tradition. There are some of these cultures that are famous not only in Pampanga but in the whole country.</p>
+                            
+                                <div class="card-footer text-center">
+                                    <a href="https://kapampangan.org/is-there-kapampangan-art/" class="btn btn-primary" target="_blank">View article</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">      
+                        <div class="card border-secondary bg-success mb-3" style="width: 25rem; height: auto;">
+                            <img src="https://thehappytrip.com/wp-content/uploads/2019/08/IMG_20190812_095842.jpg" class="card-img-top" alt="Puning Hot Spring">
+                            <div class="card-body text-white">
+                                <h4 class="card-title">Stakeholders urge reopening of Puning Hot Spring</h4>
+                                <p class="card-text">CITY OF SAN FERNANDO Tourism stakeholders are calling for the reopening of the Puning Hot Spring located in between Angeles City and Porac town.</p>
+                                <div class="card-footer text-center">
+                                    <a href="https://www.sunstar.com.ph/article/1944890/pampanga/local-news/stakeholders-urge-reopening-of-puning-hot-spring" class="btn btn-primary text-center" target="_blank">View Article</a>
+                                </div>          
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card border-secondary bg-success mb-3" style="width: 25rem; height: auto;">
+                            <img src="https://img.freepik.com/free-photo/healthy-vegetables-wooden-table_1150-38014.jpg?w=2000" class="card-img-top" alt="...">
+                            <div class="card-body text-white">
+                                <h4 class="card-title">San Simon village wins Best Gulayan Sa Barangay</h4>
+                                <p class="card-text">SAN SIMON Barangay Santa Monica of this town was named champion in Search for Best Gulayan in Pampanga.</p>
+                                <br>
+                                <div class="card-footer text-center">
+                                    <a href="https://www.sunstar.com.ph/article/1944540/pampanga/local-news/san-simon-village-wins-best-gulayan-sa-barangay" class="btn btn-primary" target="_blank">View Article</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+        <br>
+            <?php include "footer.php"; ?>
+    </body>
+</html>
