3954f6d6671818709aec3a046b853a873ce70dbc
kduterte <keduterte@gmail.com>
2024-05-02 17:11:06 +0800

    code on Food Blogs

---
+++ Blogs/Food_blogs.php
@@ -0,0 +1,230 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/blogs.css" rel="stylesheet">
+    <link href="css/food_newsletter.css" rel="stylesheet">
+    <link href="css/gallery.css" rel="stylesheet">
+    <link rel="shortcut icon" href="https://raw.githubusercontent.com/dejaaay/Cabelen/main/assets/img/Cabalen.png">
+    <title>Food Blogpage</title>
+</head>
+<body>
+
+<?php include "navbar.php"; ?>
+    <div class="mushroom1">
+      <img src="img/1.png" alt="Mushroom Icon" width="250" height="250">
+    </div>
+    <!--
+    <div class="mushroom2">
+      <img src="img/2.png" alt="Mushroom Icon" width="250" height="250">
+    </div>
+    -->
+    <div class="murcon2">
+      <img src="img/12.png" alt="Murcon Icon" width="250" height="250">
+    </div>
+    <div class="bbq1">
+      <img src="img/3.png" alt="Bbq Icon" width="250" height="250">
+    </div>
+    <div class="bbq2">
+      <img src="img/4.png" alt="Bbq Icon" width="250" height="250">
+    </div>
+    <div class="bowl1">
+      <img src="img/7.png" alt="Bowl Icon" width="250" height="250">
+    </div>
+    <div class="bowl2">
+      <img src="img/8.png" alt="Bowl Icon" width="250" height="250">
+    </div>
+    <div class="juice1">
+      <img src="img/9.png" alt="Juice Icon" width="250" height="250">
+    </div>
+    <div class="juice2">
+      <img src="img/10.png" alt="Juice Icon" width="250" height="250">
+    </div>
+    <div class="sili">
+      <img src="img/6.png" alt="Sili Icon" width="250" height="250">
+    </div>
+    
+    <div class="container px-5" id="Food">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h1 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> A foodie in Pampanga? </h1> 
+            <img src="https://gttp.imgix.net/352610/x/0/" alt="Food" class="center">
+            <div class="container p-2">
+        </div>
+        
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="Betute">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Betute </h2> 
+            <img src="https://kapampanganku.com/v3/wp-content/uploads/2019/08/Betute.jpg" alt="Betute" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Do you want to try a stuffed frog? Known as the Betute Tagak these frogs are farm-raised so they are safe to eat and stuffed with ground beef, a must to try in Apag Marangle.
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="Bringhe">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Bringhe </h2> 
+            <img src="https://i0.wp.com/www.angsarap.net/wp-content/uploads/2019/11/Bringhe-Wide.jpg?ssl=1" alt="Bringhe" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Bringhe is like a paella with a Kapampangan twist, down at Downtown Café 1956 they can serve you this specialty which is cooked with Luyang dilaw and gata topped with carrots, bell peppers and boiled eggs.
+
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="Murcon">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Murcon </h2> 
+            <img src="https://outoftownblog.com/wp-content/uploads/2015/12/Morcon-photo-by-Delmonte.ph_-600x317.jpg" alt="Murcon" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Murcon is a Kapampangan specialty that consists of ground pork & beef, hard-boiled eggs, pickles and hotdog/chorizo slices, it's mostly served at parties but why not try it for lunch at Everybody’s Café.
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>    
+    
+    <div class="container px-5" id="PakoSalad">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Pako Salad </h2> 
+            <img src="https://img-global.cpcdn.com/recipes/4dd9930ec29193b0/1200x630cq70/photo.jpg" alt="Pako Salad" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pako salad, a specialty in Abe’s Farm, gives you a fresh taste and scenery. There are plentiful of pako in Pampanga. The salad consists of paco fern, raw onion, tomato strips and salted eggs.
+
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+        
+    <div class="container px-5" id="Sisig">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Sisig </h2> 
+            <img src="https://panlasangpinoy.com/wp-content/uploads/2019/12/sizzling-crispy-sisig-recipe-.jpg" alt="Sisig" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Did you know the Kapampangan cuisine introduced sisig to the country and was later on re-invented by the known restaurant owner Aling Lucing and Sisig is mostly made out of pig head and it has been boiled, grilled and fried before being put on a sizzling plate Aling Lucing Sisig is where you should go if you ought to try the origin of the sisig.
+
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="SanNicolasCookies">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> San Nicolas Cookies </h2> 
+            <img src="https://images.summitmedia-digital.com/yummyph/images/2019/07/29/prettysannicolascookies.jpg" alt="SanNicolasCookies" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Now for some pasalubong San Nicolas Cookies is the one to take home for. Its specialness came  from its intricate embroidered like design. So don’t forget to bring some pasalubong to your loved ones and let them experience some kapampangan.
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="TibukTibuk">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Tibuk-Tibuk </h2> 
+            <img src="https://images.summitmedia-digital.com/yummyph/images/2019/06/29/susies-cuisine-tibuk-tibuk-002.jpg" alt="TibukTibuk" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        How about a dessert? Tibuk-Tibuk it is a Kapampangan’s version of a maja blanca.The main ingredient is carabao milk in which Susie’s Cuisine is where to go to try this.
+
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+    <div class="container px-5" id="TokwatBaboy">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Tokwa't Baboy </h2> 
+            <img src="https://theorchidgardens.com/wp-content/uploads/2017/11/tokwat-baboy.png" alt="TokwatBaboy" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p class="desc" style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Tokwa’t Baboy is one of the most well-known dishes in the Philippines and it is best tasted in  Pampanga. It features fried tofu with pig ears and soy sauce. If you want to try the best, then go to Mila’s Tokwa’t Baboy.
+
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <?php include "footer.php"; ?>
+</body>
+</html>
