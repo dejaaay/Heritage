1f82f15b70389308cf81bb9e78cd2255c7c505d3
ADR301 <151512964+ADR301@users.noreply.github.com>
2024-05-05 11:11:52 +0800

    Created videos.php

---
+++ videos/videos.php
@@ -0,0 +1,152 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/videos.css" rel="stylesheet"/>
+    <link href="css/video_newsletter.css" rel="stylesheet"/>
+    <link rel="shortcut icon" href="https://raw.githubusercontent.com/dejaaay/Cabelen/main/assets/img/Cabalen.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vidoes Page</title>
+</head>
+<body class="green-test-1">
+
+    <!-- NavbarCode -->
+    <?php include "../Navbar/navbar.php"; ?>
+
+    <!-- Content Section 1 -->
+    <section>
+    <div class="bg-parent">
+            <div class="bg-settings-left parol-bg-w1 rotate-right"></div>
+            <div class="bg-settings-right leaf-bg-w2"></div>
+        </div>
+            <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/howgEFee5MU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h1>Learn more about the wonders of Pampanga: The Culinary Capital of the Philippines through this video by Shelley Casao.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+
+    <!-- Content Section 2 -->
+    <section>
+    <div class="bg-parent">
+            <div class="bg-settings-right parol-bg-b2"></div>
+            <div class="bg-settings-left leaf-bg-g1"></div>
+        </div>
+            <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/mcxMnVD26FU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5">
+                            <h1>Take a quick look at the Sinukwan Festival, a weeklong festival that captures the soul of San Fernando City.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
+        </section>
+        
+        <!-- Content Section 3 -->
+        <section>
+        <div class="bg-parent">
+            <div class="bg-settings-left parol-bg-w2"></div>
+            <div class="bg-settings-right leaf-bg-w2"></div>
+        </div>
+            <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/C8q1F1KHgXE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h1>Join GMA's Biyahero Drew as he explores internationally-inspired restaurants and attractions in Pampanga.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content Section 4 -->
+        <section>
+        <div class="bg-parent">
+            <div class="bg-settings-right parol-bg-b1 rotate-left"></div>
+            <div class="bg-settings-left leaf-bg-g1"></div>
+        </div>
+            <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/I2zykJTKel4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h1>Watch GMA's Biyahe ni Drew's coverage of the San Guillermo Parish Church in Bacolor, known for it being half-buried due to a lahar overflow during Mount Pinatubo's eruption.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content Section 5 -->
+        <section>
+        <div class="bg-parent">
+            <div class="bg-settings-left parol-bg-w1 rotate-right"></div>
+            <div class="bg-settings-right leaf-bg-w2"></div>
+        </div>
+            <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/VXjrCIcGZmw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h1>See through a foreign foodie's perspective with this video made by Best Ever Food Review Show.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content Section 6 -->
+        <section>
+        <div class="bg-parent">
+            <div class="bg-settings-right parol-bg-b2"></div>
+            <div class="bg-settings-left leaf-bg-g1"></div>
+        </div>
+            <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/0s3XWPti6Rc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h1>A new Night Market just opened in San Fernando! Learn more through this GMA coverage of the Paskuhan Village Night Market.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+<?php include "../footer/footer.php"; ?>
+</html>
