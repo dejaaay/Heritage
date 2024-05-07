6b917a5aeec5e62401f2265eed11853611b967e6
kduterte <keduterte@gmail.com>
2024-05-07 17:31:00 +0800

    added header and footer on TouristDest

--- Blogs/TouristDest_blogs.php
+++ Blogs/TouristDest_blogs.php
@@ -1,59 +1,62 @@
 <?php ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="shortcut icon" href="https://raw.githubusercontent.com/dejaaay/Cabelen/main/assets/img/Cabalen.png">
-    <title> Tourist Spots Blog Page </title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Latest compiled and minified CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <!-- Latest compiled JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="shortcut icon" href="https://raw.githubusercontent.com/dejaaay/Cabelen/main/assets/img/Cabalen.png">
+  <title> Tourist Spots Blog Page </title>
 </head>
+
 <body>
-<?php include "navbar.php"; ?>  
+  <?php include "../Navbar/header.php"; ?>
 
-    <div class="pin2">
-      <img src="" alt="Pin Icon" width="250" height="250">
-    </div>
-    <div class="mountain1">
-      <img src="" alt="Mountain Icon" width="250" height="250">
-    </div>
-    <div class="mountain2">
-      <img src="" alt="Mountain Icon" width="250" height="250">
-    </div>
-    <div class="trees1">
-      <img src="" alt="Trees Icon" width="250" height="250">
-    </div>
-    <div class="trees2">
-      <img src="" alt="Trees Icon" width="250" height="250">
-    </div>
-    <div class="map1">
-      <img src="" alt="Map Icon" width="250" height="250">
-    </div>
-    
-    <div class="container px-5" id="TouristSpots">
-      <div class="row gx-5 align-items-center">
-        <div class="col-lg-12">
-            <br><br> 
-            <h1 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Tourist Spots </h1> 
-            <img src="https://www.camella.com.ph/wp-content/uploads/2021/11/image-27.png" alt="Tourist Spot Welcome - Hot Air Balloon" class="center">
-            <div class="container p-2">
+  <div class="pin2">
+    <img src="" alt="Pin Icon" width="250" height="250">
+  </div>
+  <div class="mountain1">
+    <img src="" alt="Mountain Icon" width="250" height="250">
+  </div>
+  <div class="mountain2">
+    <img src="" alt="Mountain Icon" width="250" height="250">
+  </div>
+  <div class="trees1">
+    <img src="" alt="Trees Icon" width="250" height="250">
+  </div>
+  <div class="trees2">
+    <img src="" alt="Trees Icon" width="250" height="250">
+  </div>
+  <div class="map1">
+    <img src="" alt="Map Icon" width="250" height="250">
+  </div>
+
+  <div class="container px-5" id="TouristSpots">
+    <div class="row gx-5 align-items-center">
+      <div class="col-lg-12">
+        <br><br>
+        <h1 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Tourist Spots </h1>
+        <img src="https://www.camella.com.ph/wp-content/uploads/2021/11/image-27.png" alt="Tourist Spot Welcome - Hot Air Balloon" class="center">
+        <div class="container p-2">
         </div>
-        
-        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
-           Pampanga is known for being he Culinary Capital of the Philippines which means they offer some of the best ever Filipino dishes. 
-           But Pampanga is also a getaway filled with amazing and most talked about tourist destinations. You'll surely enjoy your visit and will be an unforgettable adventure!
-         </p>   
+
+        <p style="font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+          Pampanga is known for being he Culinary Capital of the Philippines which means they offer some of the best ever Filipino dishes.
+          But Pampanga is also a getaway filled with amazing and most talked about tourist destinations. You'll surely enjoy your visit and will be an unforgettable adventure!
         </p>
-        </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        </p>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  <?php include "../footer/footer.php"; ?>
 </body>
+
 </html>
