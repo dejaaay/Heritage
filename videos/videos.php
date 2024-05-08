dc7944b9eb91e380b8415491c818bb12aaaeee44
kduterte <keduterte@gmail.com>
2024-05-09 07:15:06 +0800

    added margin-top sa videos.php and landingpage.php

--- videos/videos.php
+++ videos/videos.php
@@ -1,20 +1,27 @@
 <?php ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="css/videos.css" rel="stylesheet"/>
-    <link href="css/video_newsletter.css" rel="stylesheet"/>
+    <link href="css/videos.css" rel="stylesheet" />
+    <link href="css/video_newsletter.css" rel="stylesheet" />
     <link rel="shortcut icon" href="https://raw.githubusercontent.com/dejaaay/Cabelen/main/assets/img/Cabalen.png">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vidoes Page</title>
 </head>
+<style>
+    #row1 {
+        margin-top: 9%;
+    }
+</style>
+
 <body class="green-test-1">
 
     <!-- NavbarCode -->
@@ -22,11 +29,11 @@
 
     <!-- Content Section 1 -->
     <section>
-    <div class="bg-parent">
+        <div class="bg-parent">
             <div class="bg-settings-left parol-bg-w1 rotate-right"></div>
             <div class="bg-settings-right leaf-bg-w2"></div>
         </div>
-            <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5">
+        <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5" id="row1">
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6 order-lg-2">
@@ -40,15 +47,15 @@
                 </div>
             </div>
         </div>
-        </section>
+    </section>
 
     <!-- Content Section 2 -->
     <section>
-    <div class="bg-parent">
+        <div class="bg-parent">
             <div class="bg-settings-right parol-bg-b2"></div>
             <div class="bg-settings-left leaf-bg-g1"></div>
         </div>
-            <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
+        <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6 order-lg-1">
@@ -61,16 +68,16 @@
                     </div>
                 </div>
             </div>
-         </div>
-        </section>
-        
-        <!-- Content Section 3 -->
-        <section>
+        </div>
+    </section>
+
+    <!-- Content Section 3 -->
+    <section>
         <div class="bg-parent">
             <div class="bg-settings-left parol-bg-w2"></div>
             <div class="bg-settings-right leaf-bg-w2"></div>
         </div>
-            <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5">
+        <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5">
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6 order-lg-2">
@@ -83,15 +90,15 @@
                     </div>
                 </div>
             </div>
-        </section>
+    </section>
 
-        <!-- Content Section 4 -->
-        <section>
+    <!-- Content Section 4 -->
+    <section>
         <div class="bg-parent">
             <div class="bg-settings-right parol-bg-b1 rotate-left"></div>
             <div class="bg-settings-left leaf-bg-g1"></div>
         </div>
-            <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
+        <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6">
@@ -104,15 +111,15 @@
                     </div>
                 </div>
             </div>
-        </section>
+    </section>
 
-        <!-- Content Section 5 -->
-        <section>
+    <!-- Content Section 5 -->
+    <section>
         <div class="bg-parent">
             <div class="bg-settings-left parol-bg-w1 rotate-right"></div>
             <div class="bg-settings-right leaf-bg-w2"></div>
         </div>
-            <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5">
+        <div class="green-test-1 vidspacing container-fluid d-flex justify-content-evenly px-5">
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6 order-lg-2">
@@ -125,15 +132,15 @@
                     </div>
                 </div>
             </div>
-        </section>
+    </section>
 
-        <!-- Content Section 6 -->
-        <section>
+    <!-- Content Section 6 -->
+    <section>
         <div class="bg-parent">
             <div class="bg-settings-right parol-bg-b2"></div>
             <div class="bg-settings-left leaf-bg-g1"></div>
         </div>
-            <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
+        <div class="white-bg vidspacing container-fluid d-flex justify-content-evenly px-5">
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6">
@@ -146,7 +153,8 @@
                     </div>
                 </div>
             </div>
-        </section>
-    </body>
+    </section>
+</body>
 <?php include "../footer/footer.php"; ?>
+
 </html>
