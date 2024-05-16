235695889fdd1d99a0884920e0032bedb6274798
daniel john buco <djbuuuco@gmail.com>
2024-05-17 00:53:25 +0800

    WILL DO CHANGES ONCE GOT THE DB

--- Events/events.php
+++ Events/events.php
@@ -1,64 +1,230 @@
+<?php
+session_start();
+include '../admin/products/connect.php';
+
+?>
+
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
+<html lang="en">
+
+<head>
     <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Latest compiled and minified CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-        <!-- Latest compiled JavaScript -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-        <link href="css/events.css" rel="stylesheet">
-        <link rel="shortcut icon" href="https://raw.githubusercontent.com/dejaaay/Cabelen/main/assets/img/Cabalen.png">
-        <title>Events</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../assets/images/SK-Icon.png"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/events.css">
+   
+    <title>Events</title>
+</head>
 
-    </head>
-    
-    <body>
-    <?php include "navbar.php"; 
-    ?>
-    <ul>
-        
-        <div class="events">
-        <?php
-        include 'admin/post/connect.php';
-        $sql = "SELECT * FROM `posts`";
-        $result = mysqli_query($con, $sql);
-        $check=mysqli_num_rows($result)>0;
-        if ($check) {
-          while ($row = mysqli_fetch_assoc($result)) {
- 
-            ?>
-      
-         
-            
-            <div class="container">
-              <div class="row-lg-12">
-                <div class="col-md-4">
-                  <div class="card">
-                    <div class="card-header bg-success text-white">
-                      F e a t u r e d   N e w s
+<body>
+    <!-- Header -->
+    <div>
+        <?php include "../header/header.php"; ?>
+    </div>
+
+    <!-- Carousel -->
+    <div class="main-content-container">
+        <div class="carousel">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../assets/storeimg/MainFloor1.jpg" alt="First slide">
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $title = $row['title'];?></h5>
-                        <img src="admin/post/<?php echo $row['image']; ?>" class="card-img-bottom" alt="..." height="500" width="850">
+                    <div class="carousel-item">
+                        <img src="../assets/storeimg/2ndfloor1.jpg" alt="Second slide">
                     </div>
-                    <p class="card-text"><?php echo $description = $row['description'];?></p>
+                    <div class="carousel-item">
+                        <img src="../assets/storeimg/Front1.jpg" alt="Third slide">
                     </div>
-                  </div>
                 </div>
-              </div>
+
+                <!-- Carousel controls -->
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
+        </div>
+    </div>
+
+    <!-- Main content -->
+    <div class="container">
+        <!-- Page title -->
+        <h1 class="TitlePage">Street Kohi Events</h1>
+      </div>
+
+        <!-- Divider -->
+        <hr class="divider" />
+
+        <!-- Headings for sections -->
+    <div class="text-center mb-4">
+        <h2 class="Nameplate">Customer</h2>
+    </div>
+
+    <!-- Events section for Customers -->
+    <div class="events">
+        <div class="row">
             <?php
+            include '../admin/products/connect.php';
             
-          }
-          ;
+           
+           // Query for Customer events
+$sql_customer = "SELECT * FROM `event` WHERE `event_tag` = 'Customer'"; /// CHANGE INTO
+$result_customer = mysqli_query($con, $sql_customer);
+
+if ($result_customer && mysqli_num_rows($result_customer) > 0) {
+    while ($row = mysqli_fetch_assoc($result_customer)) {
+        // Display Customer events
+        echo '<div class="col-md-4 event-card" data-tag="' . $row['event_tag'] . '">';
+        echo '<div class="card shadow showevent" style="width: 18rem;"';
+        echo 'data-event_name="' . $row['event_name'] . '"';
+        echo 'data-event_description="' . $row['event_description'] . '"';
+        echo 'data-event_date="' . date("F j, Y", strtotime($row['event_date'])) . '"';
+        echo 'data-event_time="' . date("g:i a", strtotime($row['event_time'])) . '"';
+        echo 'data-event_price="' . $row['event_price'] . '"';
+        echo 'data-event_image="../admin/products/' . $row['event_image'] . '"';
+        echo 'data-event_venue="' . $row['event_venue'] . '">';
+        echo '<div class="card-header">';
+        echo '<h5 class="card-title">' . $row['event_name'] . '</h5>';
+        echo '</div>';
+        echo '<img class="card-img-top" src="../admin/products/' . $row['event_image'] . '" alt="Event image">';
+        
+        // Truncate the event description to a specific length (e.g., 50 characters)
+        $fullDescription = $row['event_description'];
+        $truncatedDescription = strlen($fullDescription) > 150 ? substr($fullDescription, 0, 150) . '...' : $fullDescription;
+        
+        echo '<div class="card-body">';
+        echo '<p class="card-text">' . date("F j, Y", strtotime($row['event_date'])) . ' ' . date("g:i a", strtotime($row['event_time'])) . '</p>';
+        echo '<p class="card-text">' . $truncatedDescription . '</p>'; // Display truncated description
+        echo '<p class="card-text">₱' . number_format($row['event_price'], 2) . '</p>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+    }
+}
+            ?>
+        </div>
+    </div>
+
+    <!-- Headings for sections -->
+    <div class="text-center mb-4">
+        <h2 class="Nameplate">Seller</h2>
+    </div>
+
+    <!-- Events section for Sellers -->
+    <div class="events">
+        <div class="row">
+            <?php
+           
+// Query for Seller events
+$sql_seller = "SELECT * FROM `event` WHERE `event_tag` = 'Seller'";
+$result_seller = mysqli_query($con, $sql_seller);
+
+if ($result_seller && mysqli_num_rows($result_seller) > 0) {
+    while ($row = mysqli_fetch_assoc($result_seller)) {
+        // Display Seller events
+        echo '<div class="col-md-4 event-card" data-tag="' . $row['event_tag'] . '">';
+        echo '<div class="card shadow showevent" style="width: 18rem;"';
+        echo 'data-event_name="' . $row['event_name'] . '"';
+        echo 'data-event_description="' . $row['event_description'] . '"';
+        echo 'data-event_date="' . date("F j, Y", strtotime($row['event_date'])) . '"';
+        echo 'data-event_time="' . date("g:i a", strtotime($row['event_time'])) . '"';
+        echo 'data-event_price="' . $row['event_price'] . '"';
+        echo 'data-event_image="../admin/products/' . $row['event_image'] . '"';
+        echo 'data-event_venue="' . $row['event_venue'] . '">';
+        echo '<div class="card-header">';
+        echo '<h5 class="card-title">' . $row['event_name'] . '</h5>';
+        echo '</div>';
+        
+        // Display event image
+        echo '<img class="card-img-top" src="../admin/products/' . $row['event_image'] . '" alt="Event image">';
+        
+        // Truncate the event description in the card body
+        $fullDescription = $row['event_description'];
+        $truncatedDescription = strlen($fullDescription) > 150 ? substr($fullDescription, 0, 150) . '...' : $fullDescription;
+        
+        // Display event information in the card body
+        echo '<div class="card-body">';
+        echo '<p class="card-text">' . date("F j, Y", strtotime($row['event_date'])) . ' ' . date("g:i a", strtotime($row['event_time'])) . '</p>';
+        echo '<p class="card-text">' . $truncatedDescription . '</p>'; // Display truncated description in the card
+        echo '<p class="card-text">₱' . number_format($row['event_price'], 2) . '</p>';
+        echo '</div>';
+        
+        echo '</div>';
+        echo '</div>';
+    }
+}
+            ?>
+        </div>
+    </div>
 
 
-        }
-        ;
-        ?>
-      </ul>
-      </body>
-      </html>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel"></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="card" style="width: 100%;">
+                        <img src="..." class="card-img-top" alt="..." id="img-src">
+                        <div class="card-body">
+                            <h5 class="card-title" id="date-time"></h5>
+                            <p class="card-text" id="desc"></p>
+                            <p class="card-text" id="price"></p>
+                            <p class="card-text" id="venue"></p>
+                            <a href="event-register.php" class="btn btn-primary" id="link">Open Link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- Footer -->
+<div>
+        <?php include "../footer/footer.php"; ?>
+    </div>
+
+    </body>
+<!-- JavaScript -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="../javascript/header.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+$(document).ready(function() {
+    // Event listener for clicks on event cards
+    $(document).on('click', '.event-card', function() {
+        // Retrieve the clicked event card
+        var eventCard = $(this);
+        
+        // Retrieve data attributes from the event card
+        var eventTitle = eventCard.find('.card-title').text(); // Event title
+        var eventImageSrc = eventCard.find('.card-img-top').attr('src'); // Event image source
+        var eventDesc = eventCard.find('.card-text').eq(1).text(); // Event description
+        
+        // Update modal content
+        $('#staticBackdropLabel').text(eventTitle); // Set modal title
+        $('#img-src').attr('src', eventImageSrc).attr('alt', 'Event image'); // Set modal image source
+        $('#desc').text(eventDesc); // Set modal description
+        
+        // Show the modal
+        $('#staticBackdrop').modal('show');
+    });
+});
+</script>
+
+
+</html>
+
---
+++ admin/dashboard/admin-news.php
@@ -0,0 +1,122 @@
+<?php
+include "connect.php";
+session_start();
+if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../css/archiveEvent.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <title>Appointments</title>
+        <link rel="icon" type="image/x-icon" href="../../assets/images/SK-Icon.png">
+    </head>
+
+
+    <body>
+        <!-- Dashboard Header -->
+        <div>
+            <?php include "admin-header.php"; ?>
+        </div>
+
+        <!-- Archive Event Management Section -->
+        <div class="content-section">
+            <div class="header-container center-text">
+                <h2>Appointments</h2>
+            </div>
+
+            <div class="container-flex">
+                <!-- Archive Events Table -->
+                <table class="table table-striped archive-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Contact Number</th>
+                            <th>Email</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Include database connection
+                        include('../../connect/connect.php');
+
+                        // Prepare the fetch query for archive_event table
+                        $stmt_fetch = mysqli_prepare($con, "SELECT * FROM `booking`");
+
+                        // Execute the fetch query
+                        mysqli_stmt_execute($stmt_fetch);
+
+                        // Get the result of the fetch query
+                        $result_fetch = mysqli_stmt_get_result($stmt_fetch);
+
+                        // Check if the query was successful and the archive event data exists
+                        if ($result_fetch && mysqli_num_rows($result_fetch) > 0) {
+                            // Fetch all archive event data as an associative array
+                            $appointment = mysqli_fetch_all($result_fetch, MYSQLI_ASSOC);
+
+                            // Iterate over each record and create a table row
+                            foreach ($appointment as $apptmnt) {
+
+                                echo '<tr>';
+                                echo '<td>' . htmlspecialchars($apptmnt['appointment_id']) . '</td>';
+                                echo '<td>' . htmlspecialchars($apptmnt['customer_name']) . '</td>';
+                                echo '<td>' . htmlspecialchars($apptmnt['customer_number']) . '</td>';
+                                echo '<td>' . htmlspecialchars($apptmnt['customer_email']) . '</td>';
+                                echo '<td>' . htmlspecialchars($apptmnt['appointment_date']) . '</td>';
+                                echo '<td>' . htmlspecialchars($apptmnt['appointment_time']) . '</td>';
+
+                                // Add buttons for retrieve and delete actions
+                                echo '<td>';
+
+                                // Button to delete the archive event
+                                echo '<button class="delete-button" onclick="showConfirmationModal(' . htmlspecialchars($apptmnt['appointment_id']) . ')">Delete</button>';
+                                echo '</td>';
+
+                                echo '</tr>';
+                            }
+                        } else {
+                            // Output an error message if no archive event data is found
+                            echo '<tr><td colspan="10">No archive events found.</td></tr>';
+                        }
+
+                        // Close the prepared statement
+                        mysqli_stmt_close($stmt_fetch);
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+        <!-- Add JavaScript function to handle the deletion confirmation -->
+        <script>
+            function showConfirmationModal(appointment_id) {
+                // Show the confirmation dialog
+                if (confirm("Are you sure you want to delete this appointment permanently?")) {
+                    // If the user confirms, redirect to the delete script
+                    window.location.href = 'delete_from_bookings.php?appointment_id=' + appointment_id;
+                }
+            }
+        </script>
+
+    </body>
+
+    </html>
+
+<?php
+} else {
+    header("Location: ../login/admin-login.php");
+}
+?>
