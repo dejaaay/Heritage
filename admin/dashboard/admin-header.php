948adc1c27892ea7daa89ad460f65d3cefbba001
kduterte <keduterte@gmail.com>
2024-05-18 00:23:08 +0800

    updated logout button on admin-header.php

--- admin/dashboard/admin-header.php
+++ admin/dashboard/admin-header.php
@@ -28,29 +28,72 @@
     }
 
     /* Logout button */
-    #logout-btn {
-        color: white;
-        transition: color 0.3s ease; /* Add transition for color change */
-    }
+    .Btn-logout {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 45px;
+  height: 45px;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition-duration: .3s;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
+  background-color: rgb(255, 65, 65);
+}
 
-    #logout-btn:hover {
-        color: #EE4E4E; /* Change color on hover */
-    }
-    button.logout-btn {
-      position: absolute;
-      right: 10px;
-      top: 50%;
-      transform: translateY(-50%);
-    }
+/* plus sign */
+.sign-logout {
+  width: 100%;
+  transition-duration: .3s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-    button.logout-btn.btn-danger {
-      background-color: red;
-    }
-    
-    button.logout-btn.btn-danger:active {
-      background-color: red;
-      color: red;
-    }
+.sign-logout svg {
+  width: 17px;
+}
+
+.sign-logout svg path {
+  fill: white;
+}
+/* text */
+.text-logout {
+  position: absolute;
+  right: 0%;
+  width: 0%;
+  opacity: 0;
+  color: white;
+  font-size: 1.2em;
+  font-weight: 600;
+  transition-duration: .3s;
+}
+/* hover effect on button width */
+.Btn-logout:hover {
+  width: 125px;
+  border-radius: 40px;
+  transition-duration: .3s;
+}
+
+.Btn-logout:hover .sign {
+  width: 30%;
+  transition-duration: .3s;
+  padding-left: 20px;
+}
+/* hover effect button's text */
+.Btn-logout:hover .text {
+  opacity: 1;
+  width: 70%;
+  transition-duration: .3s;
+  padding-right: 10px;
+}
+/* button click effect*/
+.Btn-logout:active {
+  transform: translate(2px ,2px);
+}
 
   </style>
 </head>
@@ -71,10 +114,12 @@
             <li><a href="admin-appointments.php" onclick="handleNavClick(event)" class="nav-links">Appointments</a></li>
             <li><a href="admin-restos.php" onclick="handleNavClick(event)" class="nav-links">Restaurants</a></li>
 
-            <button id="logout-btn" class="logout-btn btn btn btn-danger btn-lg d-flex justify-content-end">
-    <a style="color: white; text-decoration: none;" class="nav-link" href="../login/logout.php">Logout</a>
-</button>
-
+            <a class="Btn-logout" href="../login/logout.php">
+  
+  <div class="sign-logout"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
+  
+  <div class="text-logout">Logout</div>
+  </a>
   </header>
 
   <script src="../javascript/header.js"></script>
