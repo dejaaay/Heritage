b512cf02347742d154348ef2bc5ee84ece7f8c7b
Peter <peterlenard.arnoza.cics@ust.edu.ph>
2024-05-17 21:53:15 +0800

    dynamic grid

--- Events/events.php
+++ Events/events.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include '../connect/connect.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +14,6 @@ include '../connect/connect.php';
     <link rel="icon" type="image/x-icon" href="../assets/images/SK-Icon.png"> 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../css/events.css">
-   
     <title>Events</title>
 </head>
 
@@ -31,7 +29,7 @@ include '../connect/connect.php';
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="../assets/storeimg/MainFloor1.jpg" alt="First slide">
+                        <img src="../img/arayat.jpeg" alt="First slide">
                     </div>
                     <div class="carousel-item">
                         <img src="../assets/storeimg/2ndfloor1.jpg" alt="Second slide">
@@ -58,12 +56,12 @@ include '../connect/connect.php';
     <div class="container">
         <!-- Page title -->
         <h1 class="TitlePage">Street Kohi Events</h1>
-      </div>
+    </div>
 
-        <!-- Divider -->
-        <hr class="divider" />
+    <!-- Divider -->
+    <hr class="divider" />
 
-        <!-- Headings for sections -->
+    <!-- Headings for sections -->
     <div class="text-center mb-4">
         <h2 class="Nameplate">Customer</h2>
     </div>
@@ -72,43 +70,37 @@ include '../connect/connect.php';
     <div class="events">
         <div class="row">
             <?php
+            // Query for Customer events
+            $sql_customer = "SELECT * FROM `news`";
+            $result_customer = mysqli_query($con, $sql_customer);
 
-           // Query for Customer events
-$sql_customer = "SELECT * FROM `news`"; /// CHANGE INTO
-$result_customer = mysqli_query($con, $sql_customer);
-
-if ($result_customer && mysqli_num_rows($result_customer) > 0) {
-    while ($row = mysqli_fetch_assoc($result_customer)) {
-        // Display Customer events
-        echo '<div class="card shadow showevent" style="width: 18rem;"';
-        echo 'data-news_name="' . $row['news_name'] . '"';
-        echo 'data-news_description="' . $row['news_desc'] . '"';
-        echo 'data-news_date="' . date("F j, Y", strtotime($row['news_date'])) . '"';
-        echo 'data-news_time="' . date("g:i a", strtotime($row['news_time'])) . '"';
-        echo 'data-news_image="../admin/dashboard/' . $row['news_img'] . '"';
-        echo '<div class="card-header">';
-        echo '<h5 class="card-title">' . $row['news_name'] . '</h5>';
-        echo '</div>';
-        echo '<img class="card-img-top" src="../admin/dashboard/' . $row['news_img'] . '" alt="news image">';
-        
-        // Truncate the news description to a specific length (e.g., 50 characters)
-        $fullDescription = $row['news_desc'];
-        $truncatedDescription = strlen($fullDescription) > 150 ? substr($fullDescription, 0, 150) . '...' : $fullDescription;
-        
-        echo '<div class="card-body">';
-        echo '<p class="card-text">' . date("F j, Y", strtotime($row['news_date'])) . ' ' . date("g:i a", strtotime($row['news_time'])) . '</p>';
-        echo '<p class="card-text">' . $truncatedDescription . '</p>'; // Display truncated description
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
-    }
-}
+            if ($result_customer && mysqli_num_rows($result_customer) > 0) {
+                while ($row = mysqli_fetch_assoc($result_customer)) {
+                    // Display Customer events
+                    echo '<div class="col-md-4 mb-4">';
+                    echo '<div class="card shadow showevent" style="width: 100%;" data-news_name="' . $row['news_name'] . '" data-news_description="' . $row['news_desc'] . '" data-news_date="' . date("F j, Y", strtotime($row['news_date'])) . '" data-news_time="' . date("g:i a", strtotime($row['news_time'])) . '" data-news_image="../admin/dashboard/' . $row['news_img'] . '">';
+                    echo '<div class="card-header">';
+                    echo '<h5 class="card-title">' . $row['news_name'] . '</h5>';
+                    echo '</div>';
+                    echo '<div class="img-container" style="height: 200px; overflow: hidden;">';
+                    echo '<img class="card-img-top" src="../admin/dashboard/' . $row['news_img'] . '" alt="news image" style="object-fit: cover; height: 100%;">';
+                    echo '</div>';
+                    
+                    // Truncate the news description to a specific length (e.g., 150 characters)
+                    $fullDescription = $row['news_desc'];
+                    $truncatedDescription = strlen($fullDescription) > 150 ? substr($fullDescription, 0, 150) . '...' : $fullDescription;
+                    
+                    echo '<div class="card-body">';
+                    echo '<p class="card-text">' . date("F j, Y", strtotime($row['news_date'])) . ' ' . date("g:i a", strtotime($row['news_time'])) . '</p>';
+                    echo '<p class="card-text">' . $truncatedDescription . '</p>'; // Display truncated description
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
+                }
+            }
             ?>
         </div>
     </div>
-
-
-
 
     <!-- Modal -->
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -119,7 +111,8 @@ if ($result_customer && mysqli_num_rows($result_customer) > 0) {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="card" style="width: 100%;">
+                    <div class="card" style="width:
+ 100%;">
                         <img src="..." class="card-img-top" alt="..." id="img-src">
                         <div class="card-body">
                             <h5 class="card-title" id="date-time"></h5>
@@ -134,40 +127,40 @@ if ($result_customer && mysqli_num_rows($result_customer) > 0) {
         </div>
     </div>
 
-<!-- Footer -->
-<div>
+    <!-- Footer -->
+    <div>
         <?php include "../footer/footer.php"; ?>
     </div>
 
-    </body>
-<!-- JavaScript -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="../javascript/header.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script>
-$(document).ready(function() {
-    // Event listener for clicks on event cards
-    $(document).on('click', '.event-card', function() {
-        // Retrieve the clicked event card
-        var eventCard = $(this);
-        
-        // Retrieve data attributes from the event card
-        var newsname = eventCard.find('.card-title').text(); // Event title
-        var eventImageSrc = eventCard.find('.card-img-top').attr('src'); // Event image source
-        var eventDesc = eventCard.find('.card-text').eq(1).text(); // Event description
-        
-        // Update modal content
-        $('#staticBackdropLabel').text(eventTitle); // Set modal title
-        $('#img-src').attr('src', eventImageSrc).attr('alt', 'Event image'); // Set modal image source
-        $('#desc').text(eventDesc); // Set modal description
-        
-        // Show the modal
-        $('#staticBackdrop').modal('show');
-    });
-});
-</script>
-
-
+        $(document).ready(function() {
+            // Event listener for clicks on event cards
+            $(document).on('click', '.showevent', function() {
+                // Retrieve the clicked event card
+                var eventCard = $(this);
+                
+                // Retrieve data attributes from the event card
+                var newsname = eventCard.data('news_name');
+                var eventImageSrc = eventCard.data('news_image');
+                var eventDesc = eventCard.data('news_description');
+                var eventDate = eventCard.data('news_date');
+                var eventTime = eventCard.data('news_time');
+                
+                // Update modal content
+                $('#staticBackdropLabel').text(newsname);
+                $('#img-src').attr('src', eventImageSrc).attr('alt', 'Event image');
+                $('#date-time').text(eventDate + ' ' + eventTime);
+                $('#desc').text(eventDesc);
+                
+                // Show the modal
+                $('#staticBackdrop').modal('show');
+            });
+        });
+    </script>
+</body>
 </html>
-
