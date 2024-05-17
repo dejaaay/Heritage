cb775ed4cec0b0d089d248f1afe0ebe11a8cb15e
animekittyfandog <elijahwaltererice@gmail.com>
2024-05-18 00:23:49 +0800

    updated tourist info

--- Blogs/TouristDest_blogs.php
+++ Blogs/TouristDest_blogs.php
@@ -27,44 +27,225 @@
 <body>
   <?php include "../Navbar/header.php"; ?>
 
-  <div class="pin2">
-    <img src="" alt="Pin Icon" width="250" height="250">
-  </div>
-  <div class="mountain1">
-    <img src="" alt="Mountain Icon" width="250" height="250">
-  </div>
-  <div class="mountain2">
-    <img src="" alt="Mountain Icon" width="250" height="250">
-  </div>
-  <div class="trees1">
-    <img src="" alt="Trees Icon" width="250" height="250">
-  </div>
-  <div class="trees2">
-    <img src="" alt="Trees Icon" width="250" height="250">
-  </div>
-  <div class="map1">
-    <img src="" alt="Map Icon" width="250" height="250">
-  </div>
-
-  <div class="container px-5" id="TouristSpots">
-    <div class="row gx-5 align-items-center">
-      <div class="col-lg-12">
-        <br><br>
-        <h1 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Tourist Spots </h1>
-        <img src="https://www.camella.com.ph/wp-content/uploads/2021/11/image-27.png" alt="Tourist Spot Welcome - Hot Air Balloon" class="center">
-        <div class="container p-2">
+  <div class="road1">
+      <img src="../img/15.png" alt="Mountain Icon" width="250" height="250">
+    </div>
+    <div class="road2">
+      <img src="../img/16.png" alt="Mountain Icon" width="250" height="250">
+    </div>
+    <div class="pin1">
+      <img src="../img/17.png" alt="Pin Icon" width="250" height="250">
+    </div>
+    <div class="pin2">
+      <img src="../img/18.png" alt="Pin Icon" width="250" height="250">
+    </div>
+    <div class="mountain1">
+      <img src="../img/20.png" alt="Mountain Icon" width="250" height="250">
+    </div>
+    <div class="mountain2">
+      <img src="../img/19.png" alt="Mountain Icon" width="250" height="250">
+    </div>
+    <div class="trees1">
+      <img src="../img/21.png" alt="Trees Icon" width="250" height="250">
+    </div>
+    <div class="trees2">
+      <img src="../img/22.png" alt="Trees Icon" width="250" height="250">
+    </div>
+    <div class="map1">
+      <img src="../img/23.png" alt="Map Icon" width="250" height="250">
+    </div>
+    
+    <div class="container px-5" id="TouristSpots">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br> 
+            <h1 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Tourist Spots </h1> 
+            <img src="https://www.camella.com.ph/wp-content/uploads/2021/11/image-27.png" alt="Tourist Spot Welcome - Hot Air Balloon" class="center">
+            <div class="container p-2">
+        </div>
+        
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+           Pampanga is known for being he Culinary Capital of the Philippines which means they offer some of the best ever Filipino dishes. 
+           But Pampanga is also a getaway filled with amazing and most talked about tourist destinations. You'll surely enjoy your visit and will be an unforgettable adventure!
+         </p>   
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <p style="font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
-          Pampanga is known for being he Culinary Capital of the Philippines which means they offer some of the best ever Filipino dishes.
-          But Pampanga is also a getaway filled with amazing and most talked about tourist destinations. You'll surely enjoy your visit and will be an unforgettable adventure!
+    <div class="container px-5" id="AquaPlanet">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Aqua Planet </h2> 
+            <img src="https://1.bp.blogspot.com/-yvVKlOBax5Y/XNLbbT9BIsI/AAAAAAAAzjE/L9Rnis46p6wPV6vAHTDjZs0gsEJXg7E-ACLcBGAs/s1600/aquaplanet.jpg" alt="aquaplanet" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+            Aqua Planet is a 10 hectare property located in the Clark Freeport Zone in Mabalacat Pampanga. 
+            It is the largest Waterpark in the Country which opened last 2018. Aqua Planet is located 100km from the Metro or around an hour and a half by car. 
+            With 12 different attractions to choose from there’s no way that you would be bored. Spend a day at Clark, Pampanga's Aqua Planet Waterpark with your family and friends! 
+            Aqua Planet, a 10-hectare water theme park with thrilling thrills suitable for the whole family, is only a two-hour drive from Manila. 
+            Spend the day exploring the 38 attractions and slides that make up the theme park, which range from the exhilarating Super Bowl and Tornado to the tranquil 
+            Wave River and Bubble Base. A 10-hectare water park with over 38 water slides and activities for visitors of all ages, Aqua Planet is regarded as one of the largest and most cutting-edge world-class water parks in Asia.             
         </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="Pinatubo">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Mt. Pinatubo Crater Lake </h2> 
+            <img src="https://gttp.imgix.net/308790/x/0/?auto=format%2Ccompress&crop=faces%2Cedges%2Ccenter&bg=%23fff&fit=crop&q=35&h=504&dpr=1" alt="Pinatubo" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+            The Lake Crater is located in the borders of  Pampanga, Zambales and Tarlac a little over 3 hours away from the metro. Is a lake similar to the Taal Lake Located in Batangas wherein the lake was formed when the volcano erupted in 1991 wherein there are multiple hiking trails situated along the lake. 
+            Its magnificent caldera or crater is surrounded by enormous, alpine-like rock formations, and a brilliant turquoise lake that is very stunning. Quickly becoming one of the most popular tourist destinations around Manila, Mt. Pinatubo attracted a lot of hikers.
+            
+            The greatest time to admire and take in Mt. Pinatubo's grandeur is during the dry and hot months from December to May. While trekking is permitted throughout the year, it is not advised to travel during the typhoon season from June to September because of the potential for lahar floods in the region.
+            
+            
         </p>
-      </div>
-    </div>
-  </div>
-  </div>
-  </div>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="Arayat">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Mt. Arayat National Park </h2> 
+            <img src="https://www.ourworldinwords.com/wp-content/uploads/2014/12/P1013015.jpg" alt="Mt. Arayat National Park" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+            The National Park Located 2 hours from the Metro Located in Arayat Pampanga. This mountain offers a view of Zambales and Bataan to the west and the Sierra Madre Mountain Range to the east. 
+            The mysterious Mount Arayat is located in the Pampanga towns of Arayat and Magalang, and is one of the parks that are considered to be the easiest to get to from Manila. The second-largest river on Luzon, the Pampanga,
+            runs through the provinces of Bulacan, Pampanga, and Nueva Ecija and has a circular volcano crater at its peak. Mt. Arayat, known for its crystal-clear water falling from its waterfalls and for its cool surrounds, was first declared a Forest Reserve in 1921 before being 
+            named a National Park in 1933.on's middle plains like a lone giant is the renowned Mt. Arayat. Before it caught the eye of trekkers, it was well-known in myth.
+            
+            
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>    
+    
+    <div class="container px-5" id="CandabaSwamp">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Candaba Swamp </h2> 
+            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/ab/e8/4b/egrets.jpg?w=1200&h=-1&s=1" alt="Candaba Swamp" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+            The Candaba Swamp located around 60km from the metro, is one of the premier birdwatching spots in the country wherein they make a trip from the months of October to February to get away from the harsh winter from Northern China and Parts of Siberia in Russia. 
+            This place is also home to the Candaba Bird Sanctuary with an Entrance fee of 150 Php which goes directly towards the conservation effort for the swamp.
+            Once upon a time, the Candaba Swamp was a very significant duck staging and wintering place, especially in the months of October and November when the swamp frequently hosted between 5,000 and 10,000 birds. About 100,000 ducks were spotted in one day in 1982.
+            
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+        
+    <div class="container px-5" id="AquaBeachClub">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Aqua Beach Club </h2> 
+            <img src="https://fieldsplazasuites.com/wp-content/uploads/2017/06/party-Aqua-Beach-Club3.jpg" alt="Sisig" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+            The three-story Aqua has one of the most cutting-edge and distinctive club ideas, offering a wide range of amazing party amenities and settings 
+            for even the most refined partygoers. Explore Aqua Beach Club's fanciful grounds to discover its top-notch wellness, exercise, food & beverage, merchandise, entertainment, and party options.
+            Enter one of Asia's most extraordinary clubs, change into your bathrobe or swimsuit, lock your money and valuables in the provided locker, and then explore Aqua Beach Club's fantastical grounds to discover its top-notch wellness, fitness, food & beverage, merchandise, entertainment, and party options.
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="DinosaursIsland">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Dinosaurs Island </h2> 
+            <img src="https://dhdnzx78tqry5.cloudfront.net/uploads/deal/thumb/122785.jpg" alt="DinosaursIsland" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+            An animatronic theme park and museum called Dinosaurs Island was created to provide tourists with a distinctive 
+            interactive learning experience of the dinosaurs' world with life-size creatures. It is located in Pampanga's Clark Freeport Zone. This spot contains 2 main attractions where visitors can engage in distinctive educational entertainment.
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="MuseoNingAngeles">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Museo Ning Angeles </h2> 
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Angeles_City_Pampanga_09.jpg" alt="MuseoNingAngeles" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+            Pampanga's province of Angeles City is home to a thriving cultural community. Museo Ning Angeles, or the Angeles City Museum, is the only place where all the emblems of the Kapampangan way of life are kept together. 
+            The museum was erected in 1999 inside the town hall, which was first constructed in 1922. The management of the city museum is done by The Kuliat Foundation Inc., whose "only mission is to preserve the history, culture, and tradition of Angeles City." Additionally, it serves as a location for exhibitions, art classes, concerts, and other customary occasions. Museo Ning Angeles was designated as a "Important Cultural Property of the Philippines" by the National Museum of the Philippines in 2012.
+
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+    <div class="container px-5" id="PuningHotSpring">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Puning Hot Spring </h2> 
+            <img src="https://thehappytrip.com/wp-content/uploads/2019/08/IMG_20190812_095842.jpg" alt="PuningHotSpring" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+            One of the natural hot springs produced by Mt. Pinatubo's eruption in 1991 is the Puning Hot Spring, which is situated in the province of Pampanga. The hot spring is situated in Porac, a village at the base of a volcano surrounded by 
+            lush mountain ranges. The phrase "punang" or "mapunang," which means "full of trees," was the source of the location's name before it was changed to its current form. The first settlers of the area following the eruption, the hamlet of Aetas, are in charge of maintaining the hot spring. Visitors can stay in a number of cottages inside the hot spring. In the neighborhood, there are also restrooms and a shower facility. The hot water in the pools at Puning ranges in temperature from 40 degrees to 70 degrees. The water comes from the Sacobia River and is naturally heated by the active volcano because it is close by. The location also has pools with cold water for those who like it.
+          
+        </p>
+        <br><br>  
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
   <?php include "../footer/footer.php"; ?>
 </body>
 
