0a15056a3a2b5d7d5982360bd3e23f1bcc669716
kduterte <keduterte@gmail.com>
2024-05-18 11:02:02 +0800

    unified the color scheme pt 2

--- admin/dashboard/create-restos.php
+++ admin/dashboard/create-restos.php
@@ -77,24 +77,42 @@ if (isset($_POST['submit']) && isset($_POST['restos_name'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Restos Creation</title>
-  <link rel="icon" type="image/x-icon" href="../../assets/images/SK-Icon.png">
-  <link rel="stylesheet" href="../../css/addnews.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <link rel="shortcut icon" href="../../assets/images/SK-Logo1.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restos Creation</title>
+    <link rel="icon" type="image/x-icon" href="../../assets/images/SK-Icon.png">
+    <link rel="stylesheet" href="../../css/addnews.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../../assets/images/SK-Logo1.png">
 </head>
+<style>
+    body {
+        background-color: #FFF2E1;
+    }
+
+    .event-input-container {
+        background-color: #D1BB9E;
+    }
+
+    .input-container {
+        background-color: #D1BB9E;
+    }
+
+    .input-image-container .label {
+        background-color: #D1BB9E;
+    }
+</style>
+
 <body>
-<div>
-    <?php include "admin-header.php" ?>
-</div>
-<div class="main-content-container">
-    <div class="event-form-container">
-        <div class="event-input-container">
-            <h1 class="form-header">Add Restos</h1>
+    <div>
+        <?php include "admin-header.php" ?>
+    </div>
+    <div class="main-content-container">
+        <div class="event-form-container">
+            <div class="event-input-container">
+                <h1 class="form-header">Add Restos</h1>
                 <form action="" method="post" enctype="multipart/form-data" onsubmit="return validateForm()">
                     <div class="input-container">
                         <input type="text" class="input form-control" id="restosname" name="restos_name" required>
@@ -115,15 +133,15 @@ if (isset($_POST['submit']) && isset($_POST['restos_name'])) {
 
                     <div class="input-image-container">
                         <label for="restosImage" class="label">
-                        <button id="fileButton" type="button">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 3H12H8C6.34315 3 5 4.34315 5 6V18C5 19.6569 6.34315 21 8 21H11M13.5 3L19 8.625M13.5 3V7.625C13.5 8.17728 13.9477 8.625 14.5 8.625H19M19 8.625V11.8125" stroke="#fffffff" stroke-width="2"></path>
-                            <path d="M17 15V18M17 21V18M17 18H14M17 18H20" stroke="#fffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                        </svg>
-                        Add Image
-                        </button>
-                    </label>
-                    <input class="input form-control" type="file" id="restosimg" name="restos_img" accept="image/jpeg, image/jpg, image/png" required>
+                            <button id="fileButton" type="button">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 3H12H8C6.34315 3 5 4.34315 5 6V18C5 19.6569 6.34315 21 8 21H11M13.5 3L19 8.625M13.5 3V7.625C13.5 8.17728 13.9477 8.625 14.5 8.625H19M19 8.625V11.8125" stroke="#fffffff" stroke-width="2"></path>
+                                    <path d="M17 15V18M17 21V18M17 18H14M17 18H20" stroke="#fffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                Add Image
+                            </button>
+                        </label>
+                        <input class="input form-control" type="file" id="restosimg" name="restos_img" accept="image/jpeg, image/jpg, image/png" required>
                     </div>
 
                     <div class="buttons-container">
@@ -134,74 +152,69 @@ if (isset($_POST['submit']) && isset($_POST['restos_name'])) {
             </div>
         </div>
     </div>
-</div>
+    </div>
 
-<div id="errorPopup" class="popup">
-  <div class="popup-content">
-    <span class="close" onclick="closePopup()">&times;</span>
-    <p id="errorMessage"></p>
-    <center>
-      <dotlottie-player src="https://lottie.host/23175b72-edf2-4781-a175-7e7ca03fc3ab/t4kdPVrxsW.json" background="transparent" speed="1" style="width: 150px; height: 150px;" loop autoplay></dotlottie-player>
-    </center>
-  </div>
-</div>
+    <div id="errorPopup" class="popup">
+        <div class="popup-content">
+            <span class="close" onclick="closePopup()">&times;</span>
+            <p id="errorMessage"></p>
+            <center>
+                <dotlottie-player src="https://lottie.host/23175b72-edf2-4781-a175-7e7ca03fc3ab/t4kdPVrxsW.json" background="transparent" speed="1" style="width: 150px; height: 150px;" loop autoplay></dotlottie-player>
+            </center>
+        </div>
+    </div>
 
-<script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
-<script>
-function validateForm(event) {
-    var restosname = document.getElementById('restosname').value;
-    var restosdesc = document.getElementById('restosdesc').value;
-    var restosoprtn = document.getElementById('restosoprtn').value;
-    var restoslink = document.getElementById('restoslink').value;
-    var imageInput = document.getElementById('restosimg');
-    var imageFile = imageInput.files[0];
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+    <script>
+        function validateForm(event) {
+            var restosname = document.getElementById('restosname').value;
+            var restosdesc = document.getElementById('restosdesc').value;
+            var restosoprtn = document.getElementById('restosoprtn').value;
+            var restoslink = document.getElementById('restoslink').value;
+            var imageInput = document.getElementById('restosimg');
+            var imageFile = imageInput.files[0];
 
-    if (
-        restosname.trim() === '' ||
-        restosdesc.trim() === '' ||
-        restosoprtn.trim() === '' ||
-        restoslink.trim() === '' ||
-        !imageFile
-    ) {
-        displayError('Please input all fields');
-        return false;
-    }
+            if (
+                restosname.trim() === '' ||
+                restosdesc.trim() === '' ||
+                restosoprtn.trim() === '' ||
+                restoslink.trim() === '' ||
+                !imageFile
+            ) {
+                displayError('Please input all fields');
+                return false;
+            }
 
-    // Validate image format
-    var allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
-    if (!allowedExtensions.exec(imageFile.name)) {
-        displayError('Please upload an image with .jpg, .jpeg, or .png extension.');
-        return false;
-    }
+            // Validate image format
+            var allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+            if (!allowedExtensions.exec(imageFile.name)) {
+                displayError('Please upload an image with .jpg, .jpeg, or .png extension.');
+                return false;
+            }
 
-    // If all validations pass, submit the form
-    return true;
-}
+            // If all validations pass, submit the form
+            return true;
+        }
 
-function displayError(message) {
-    var errorMessage = document.getElementById('errorMessage');
-    errorMessage.textContent = message;
-    var popup = document.getElementById('errorPopup');
-    popup.style.display = 'block';
-}
+        function displayError(message) {
+            var errorMessage = document.getElementById('errorMessage');
+            errorMessage.textContent = message;
+            var popup = document.getElementById('errorPopup');
+            popup.style.display = 'block';
+        }
 
-function closePopup() {
-    var popup = document.getElementById('errorPopup');
-    popup.style.display = 'none';
-}
+        function closePopup() {
+            var popup = document.getElementById('errorPopup');
+            popup.style.display = 'none';
+        }
 
-function goBack() {
-    window.location.href = 'admin-restos.php';
-}
-</script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-    integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
+        function goBack() {
+            window.location.href = 'admin-restos.php';
+        }
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
+
 </html>
